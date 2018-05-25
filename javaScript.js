63b3ebbd88d11f76030a9100afa29f4b1dfd1e08
Karla Ruan <karla.ruan@cceo.com.mx>
2018-05-25 13:17:07 -0500

    Ejercicios del bloque 1 con el switch funcionando

--- javaScript.js
+++ javaScript.js
@@ -3,24 +3,32 @@ console.log("2)Suma, resta, multiplicacion y division");
 console.log("3)Juego del número mágico");
 let op=prompt("Elige una opción");
 
-switch (op){
-	case 1: let n1 = prompt();
-			if(n1 %2 != 0 )
+switch (parseInt(op)){
+	case 1: let n = prompt();
+			if(n %2 != 0 )
 			console.log("El numero es impar");
 			else console.log("El numero es par");
 		break;
-	case 2: n1=prompt("Ingresa un numero");
+	case 2: let n1=prompt("Ingresa un numero");
 			let n2=prompt("Ingresa otro numero");
-			let suma=parseInt(n1)+parseInt(n2);
-			console.log("La suma es ",suma);
+			let resul=parseInt(n1)+parseInt(n2);
+			console.log("La suma es ",resul);
+			resul=parseInt(n1)-parseInt(n2);
+			console.log("La resta es ",resul);
+			resul=parseInt(n1)*parseInt(n2);
+			console.log("La multiplicacion es ",resul);
+			resul=parseInt(n1)/parseInt(n2);
+			console.log("La division es ",resul);
 		break;
 	case 3: const num=10;
-			let n;
+			let number=0;
 			do{
-			let n=prompt("Ingresa un numero");
-			n=parseInt(n);
+			number=prompt("Ingresa un numero");
+			number=parseInt(number);
 
-				if(n>num) console.log("El numero es menor");
-				else console.log("El numero es mayor");
-			}while(n==num);
+				if(number>num) console.log("El numero es menor");
+				else if (number<num) console.log("El numero es mayor");
+					else console.log("Adivinaste el numero");
+			}while(number!=num);
+		break;
 }
